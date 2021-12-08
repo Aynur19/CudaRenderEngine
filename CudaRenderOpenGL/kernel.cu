77f47bb2c8f85d3a82abfa29b898fbdf3723155e
Aynur19 <nasybullin.aynur@yandex.ru>
2021-12-09 01:01:35 +0300

    created project

---
+++ CudaRenderOpenGL/kernel.cu
@@ -0,0 +1,25 @@
+﻿#include <Windows.h> // нужно, чтобы библиотеки OpenGL подключались без ошибок
+
+#include <stdio.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <cuda_gl_interop.h>
+
+
+
+
+class CudaGlBuffer
+{
+    cudaGraphicsResource* graphRes;
+};
+
+
+int main()
+{
+    cudaGLSetGLDevice(0);
+
+    return 0;
+}
+
